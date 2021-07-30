dcd63a5a52277063675a83370365415cf0343b04
bnaoe <bp.naoe@gmail.com>
2021-07-29 21:22:27 -0500

    Add project files.

---
+++ StackOverFlowPost/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace StackOverFlowPost
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var isValidState = true;
+            var stackPost = new StackPost("Encapsulation and access modifiers", DateTime.Now);
+            stackPost.Description = "Learning encapsulation, encapsulating data and behavior around the data";
+            
+
+            while (isValidState)
+            {
+                Console.WriteLine("Type 'up' or 'down' to vote and 'exit' to exit");
+                var input = Console.ReadLine();
+
+                switch (stackPost.ValidateEntry(input))
+                {
+                    case "up": case "down": case "try":
+                        break;
+
+                    case "exit":
+                        isValidState = false;
+                        break;
+                }
+
+            }
+
+            Console.WriteLine("Title: " + stackPost.Title);
+            Console.WriteLine("Description: " + stackPost.Description);
+            Console.WriteLine("Time Posted: " + stackPost.DateTimestamp);
+            Console.WriteLine("No. of up votes: " + stackPost.GetUpVotes);
+            Console.WriteLine("No. of down botes: " + stackPost.GetDownVotes);
+            Console.WriteLine("Total no. of votes: " + stackPost.GetVotes);
+
+
+        }
+
+
+     
+}
+}
---
+++ StackOverFlowPost/StackPost.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace StackOverFlowPost
+{
+    public class StackPost
+    {
+        private readonly string _title;
+        private readonly DateTime _dateTimeStamp;
+
+        private int _upVotes;
+        private int _downVotes;
+
+        public string Description { get; set; }
+        //{
+        //    set { _description = value; }
+        //    get { return _description; }
+        //}
+
+        public string Title { get => _title; }
+
+        public DateTime DateTimestamp { get => _dateTimeStamp; }
+
+        public StackPost(string title, DateTime dateTimeStamp)
+        {
+            this._title = title;
+            this._dateTimeStamp = dateTimeStamp;
+        }
+
+        public string ValidateEntry(string input)
+        {
+            if (input.ToLower() == "up")
+            {
+                UpVote();
+                return "up";
+            }
+            else if (input.ToLower() == "down")
+            {
+                DownVote();
+                return "down";
+            }
+            else if (input.ToLower() == "exit")
+            {
+                return "exit";
+            }
+            else return "try";
+        }
+
+        public void UpVote()
+        {
+            this._upVotes++;
+        }
+
+        public void DownVote()
+        {
+            this._downVotes++;
+        }
+
+        public int GetUpVotes { get => _upVotes; }
+
+        public int GetDownVotes { get => _downVotes; }
+
+        public int GetVotes { get => _upVotes + _downVotes; }
+    }
+}
